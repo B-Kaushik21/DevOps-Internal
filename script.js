8cc62f1cf11de808b077a76d0c5d16d5c77e4783
B-Kaushik21 <boddula.kaushik@gmail.com>
2025-09-14 13:48:14 +0530

    feature lo index,style,script

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return a + b;
+}
+console.log(add(2,3));
+
+function subtract(a,b){
+    return a - b;
+}
+console.log(subtract(5,2));
+function multiply(a,b){
+    return a * b;
+}
+console.log(multiply(3,4));
+function divide(a,b){
+    return a / b;
+}
+console.log(divide(10,2));
+
+function modulus(a,b){
+    return a % b;
+}   
+console.log(modulus(10,3));
+function exponent(a,b){
+    return a ** b;
+}
+console.log(exponent(2,3));
+
+function increment(a){
+    return ++a;
+}
+
+console.log(increment(5));
+function decrement(a){
+    return --a;
+}
+console.log(decrement(5));
+
